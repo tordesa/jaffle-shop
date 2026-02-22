3a816e1ef0264c3e726bf04edf2625360a21e6ee
anthony omoijahe <anthonyomoijahe@gmail.com>
2026-02-22 12:35:23 +0000

    add jaffle_shop source yaml  file

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -2,5 +2,6 @@ select
             ID as customer_id,
             FIRST_NAME as first_name,
             LAST_NAME as last_name
-        from dbt-learn-488118.jaffle_shop.customers
+    from {{ source('raw_data', 'customers') }}
+        -- from dbt-learn-488118.jaffle_shop.customers
 
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -3,5 +3,6 @@ select
             USER_ID as customer_id,
             ORDER_DATE as order_date,
             STATUS as status
+    from {{ source('raw_data', 'orders') }}
 
-        from dbt-learn-488118.jaffle_shop.orders
+        -- from dbt-learn-488118.jaffle_shop.orders
